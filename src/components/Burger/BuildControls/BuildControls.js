9cac3f65593ae0567a725a1e3458730b511cc27a
Lindo Sebastian <lindosebastian@gmail.com>
2018-06-14 23:34:44 -0500

    burger project add ingredients functionality

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,7 +11,7 @@ const controls = [
 ];
 
 const buildControls = (props) => (
-    <div className={classes.BuildControlls}>
+    <div className={classes.BuildControls}>
         <p>Current Price: <strong>{props.price.toFixed(2)}</strong> </p>
         {
             controls.map(ctrl => (
